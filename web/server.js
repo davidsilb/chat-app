474b5880bc2be3bf3a6bcd1d38bc89f3e827ed04
Ruslan <rskorokhod23@mail.wou.edu>
2025-04-23 20:42:36 -0700

    Add exportTxt route, update index.html and server.js

--- web/server.js
+++ web/server.js
@@ -7,6 +7,7 @@ import fetch from "node-fetch";
 import path from "path";
 import { fileURLToPath } from "url";
 import ChatSession from "./mongo/ChatSession.js";
+import exportTxtRouter from "./routes/exportTxt.js";
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
@@ -26,6 +27,7 @@ app.get("/ping", (_req, res) => res.send("pong"));
 
 app.use(express.static(__dirname));
 app.use(express.json());
+app.use("/api", exportTxtRouter);
 
 const textModels = [
   { route: "compound-beta-mini", model: "compound-beta-mini" },
